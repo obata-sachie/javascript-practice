e262ca2531f076ea4937a6935527ff69df75ef04
obata-sachie <123298164+obata-sachie@users.noreply.github.com>
2023-02-07 10:30:06 +0900

    コメントの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,10 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+  // エラーになるため、実行させないようにコメントする。
+  /*
+  expect(sum(1, 2)).toBe(4);
+  */
+});
