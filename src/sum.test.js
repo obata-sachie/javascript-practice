d1298dd092418e7505fda3657730659337ba96e2
obata-sachie <123298164+obata-sachie@users.noreply.github.com>
2023-03-19 20:45:52 +0900

    文字列の練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -19,16 +19,35 @@ test('constの練習', () => {
 test('letの練習', () => {
   let bookTitle;
   expect(bookTitle).toBe(undefined); 
+
   let bookPrice = "3000";
   bookTitle="本";
   expect(bookTitle).toBe("本"); 
   expect(bookPrice).toBe("3000"); 
+
   bookPrice = "5000";
   expect(bookPrice).toBe("5000"); 
 });
 test('console.logの練習', () => {
 const total=42+42;
 console.log(total); // => 84
+
 const value = "値";
 console.log(value); 
+});
+test('文字列の練習', () => {
+  const str1 = "文字";
+  const str2 = '文字';
+  expect(str1).toBe(str2); 
+  expect(str1===str2).toBe(true); 
+  const str3 = `あああ
+いいい
+ううう`;
+  const str4 = "あああ\nいいい\nううう";
+  expect(str3===str4).toBe(true); 
+
+  const str5 = "これは${str1}です"; 
+  const str6 = `これは${str1}です`; 
+  expect(str5).toBe("これは${str1}です"); 
+  expect(str6).toBe("これは文字です"); 
 });
