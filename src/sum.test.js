82e44d72d8bc56c78599fde68a069dbbdfb27c72
obata-sachie <123298164+obata-sachie@users.noreply.github.com>
2023-03-19 18:19:36 +0900

    constの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -4,9 +4,15 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('プログラムコメントの練習', () => {
   // エラーになるため、実行させないようにコメントする。
   /*
   expect(sum(1, 2)).toBe(4);
   */
+});
+test('constの練習', () => {
+  const bookTitle = "本";
+  const bookPrice = "3000";  
+  expect(bookTitle).toBe("本"); 
+  expect(bookPrice).toBe("3000"); 
 });
